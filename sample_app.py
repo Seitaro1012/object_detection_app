884eadf924b6d0d7331388a4e745cab6a38d2850
Seitaro Miyashita <69081870+Seitaro1012@users.noreply.github.com>
2021-09-27 23:38:30 +0900

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -14,10 +14,6 @@ import json
 from numpy import save
 import streamlit as st
 
-
-with open('secret.json') as f:
-    secret = json.load(f)
-
 KEY = st.secrets['KEY']
 ENDPOINT = st.secrets['ENDPOINT']
 
